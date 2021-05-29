4d89e60acd6e8ba50be4bf17f0ee8b978a696ef2
dalhy <zsmithgod@gmail.com>
2021-05-29 18:28:18 -0300

    changes

--- Source/Events/Ready.py
+++ Source/Events/Ready.py
@@ -1,3 +1,5 @@
+from asyncio import sleep
+
 from discord import Status, Activity
 from discord.ext.commands import Cog
 
@@ -7,15 +9,13 @@ class Ready(Cog):
 
     @Cog.listener()
     async def on_ready(self):
-        print(f"-> Clasher Status: Running...")
+        print(f"-> Clasher Status: Started")
         
-        await self.client.change_presence(
-            status=Status.dnd,
-            activity=Activity(
-                name="Clash Royale",
-                type=1
-            )
-        )
+        while True:
+            await self.client.change_presence(status=Status.dnd, activity=Activity(name="Clash Royale", type=1))
+            sleep(30)
+            await self.client.change_presence(status=Status.dnd, activity=Activity(name="Type !!help for help", type=3))
+            sleep(30)
         
 def setup(client):
     client.add_cog(Ready(client))
--- Source/Help.py
+++ Source/Help.py
@@ -12,30 +12,28 @@ class Help(Cog):
     @cooldown(1, 5, BucketType.user)
     async def help(self, ctx):
         try:
-                embed = Embed(
-                    color=int(data["color"], 16),
-                    title=":crossed_swords: Clasher"
-                )
+            embed = Embed(
+                color=int(data["color"], 16),
+                title=":crossed_swords: Clasher"
+            )
                 
-                embed.add_field(
-                    name="** Comnands **",
-                    value=f"{data['emojis']['text']} **help** - Get Help" + "\n" + f"{data['emojis']['text']} **player** - Get a player's information"
-                )
+            embed.add_field(
+                name="** Comnands **",
+                value=f"{data['emojis']['text']} **help** - Get Help" + "\n" + f"{data['emojis']['text']} **player** - Get a player's information"
+            )
                 
-                embed.add_field(
-                    name="** News **",
-                    value=f"{data['emojis']['mega']} No news"
-                )
+            embed.add_field(
+                name="** News **",
+                value=f"{data['emojis']['mega']} No news"
+            )
                 
-                embed.set_thumbnail(url=ctx.author.avatar_url)
-                embed.set_footer(text=ctx.author.name)
+            embed.set_thumbnail(url=ctx.author.avatar_url)
+            embed.set_footer(text=ctx.author.name)
                 
-                await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
                 
         except Exception as e:
             print(f"-> Help: {e}")
             
-            await ctx.send(f"{data['emojis']['error']} Error. Exception log has emmited.")
-
 def setup(client):
     client.add_cog(Help(client))
